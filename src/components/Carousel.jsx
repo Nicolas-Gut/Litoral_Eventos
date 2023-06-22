fe2e426df5667f0771a4d4021dc098f4721486df
Gabrielle1sbn <gabriellesbn15@gmail.com>
2023-06-22 17:19:28 -0300

    carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,25 +1,35 @@
+import './Carousel.css'
 import { Carousel } from 'react-responsive-carousel'; 
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import foto1 from './assets/img/1f.jpg'
-import foto2 from './assets/img/2f.jpg'
-import foto3 from './assets/img/3f.jpg'
+import foto1 from './assets/img/I1.jpg'
+import foto2 from './assets/img/I2.jpg'
+import foto3 from './assets/img/I3.jpg'
+import foto4 from './assets/img/I4.jpg'
+import foto5 from './assets/img/I5.jpg'
 
 const App = () => {
     return (
-      <div sx={{width: '50%', margin: '100px', height:'50%'}}>
-        <h1>Meu Carousel</h1>
-        <Carousel >
+      <div className='container'>
+        <Carousel autoPlay={true} interval={2000}>
           <div>
             <img src={foto1} alt="Slide 1" />
-            <p className="legend">Slide 1</p>
+            <p className="legend">Slide Teste1</p>
           </div>
           <div>
             <img src={foto2} alt="Slide 2" />
-            <p className="legend">Slide 2</p>
+            <p className="legend">Slide Teste2</p>
           </div>
           <div>
             <img src={foto3} alt="Slide 3" />
-            <p className="legend">Slide 3</p>
+            <p className="legend">Slide Teste3</p>
+          </div>
+          <div>
+            <img src={foto4} alt="Slide 4" />
+            <p className="legend">Slide Teste4</p>
+          </div>
+          <div>
+          <img src={foto5} alt="Slide 5" />
+            <p className="legend">Slide Teste5</p>
           </div>
         </Carousel>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ import Footer from '../components/Footer'
 import Carousel from '../components/Carousel'
 
 
+
 const Home = () => {
   return (
     <>

26c4773c5900d1f4ac2dcc219cf055bfb40b7427
Gabrielle1sbn <gabriellesbn15@gmail.com>
2023-06-24 16:16:58 -0300

    mudei a logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 import './Footer.css'
 import Box from '@mui/material/Box'
-import Logo from './assets/img/logo2.png'
+import Logo from './assets/img/logoO2.png'
 
 const Footer = () => {
     return (
         <Box component='footer'
          sx={{
-            background: '#C0C0C0',
+            background: 'rgb(76 87 108 / 66%)',
             padding: '120px'
         }}>
-        <img src={Logo} alt='logo Litoral Eventos' style={{width: '15%'}} />
+        <img src={Logo} alt='logo Litoral Eventos' style={{width: '15%', marginBottom: '-36px'}} />
         <p>© Copyright 2023 LitoralEventos | Todos os Direitos Reservados</p>
         
             <a href='/'> Home</a>

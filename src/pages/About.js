2129cc1cbb7a5353164fc33c4c37fe994348a945
Gabrielle1sbn <gabriellesbn15@gmail.com>
2023-06-24 16:20:13 -0300

    comentei

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,24 +1,24 @@
-import Box from '@mui/material/Box'
-import Header from '../components/Header'
-import Sidebar from '../components/Sidebar'
-import Content from '../components/Content'
-import Footer from '../components/Footer'
+// import Box from '@mui/material/Box'
+// import Header from '../components/Header'
+// import Sidebar from '../components/Sidebar'
+// import Content from '../components/Content'
+// import Footer from '../components/Footer'
 
-const About = () => {
-  return (
-    <>
-      <Header />
-      <Box sx={{
-        display: 'flex',
-      }}>
-        <Sidebar />
-        <Content title="Quem Somos">
-            <p>Esta é a página de Quem Somos</p>
-        </Content>
-      </Box>
-      <Footer />
-    </>
-  )
-}
+// const About = () => {
+//   return (
+//     <>
+//       <Header />
+//       <Box sx={{
+//         display: 'flex',
+//       }}>
+//         <Sidebar />
+//         <Content title="Quem Somos">
+//             <p>Esta é a página de Quem Somos</p>
+//         </Content>
+//       </Box>
+//       <Footer />
+//     </>
+//   )
+// }
 
-export default About
+// export default About

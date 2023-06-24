dc27c2706903f8e05e7b92da98e6679a70b3c9d5
Gabrielle1sbn <gabriellesbn15@gmail.com>
2023-06-24 16:17:30 -0300

    mexer ainda

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -12,26 +12,32 @@ const ImageSlider = () => {
     speed: 500,
     slidesToShow: 3, // Exibir 3 imagens por vez (ajuste conforme necessário)
     slidesToScroll: 1,
-    centerMode: true,
-    variableWidth: true,
+    variableWidth: true
   };
 
   return (
-    <div className="slider-container">
-      <Slider {...settings}>
-        <div className="slider-item">
-          <img src={I1} alt="Imagem 1" className="circular-image" />
-        </div>
-        <div className="slider-item">
-          <img src={I2} alt="Imagem 2" className="circular-image" />
-        </div>
-        <div className="slider-item">
-          <img src={I3} alt="Imagem 3" className="circular-image" />
-        </div>
-        {/* Adicione mais imagens conforme necessário */}
-      </Slider>
-    </div>
-  );
+      <div className="slider-container">
+        <Slider {...settings}>
+          <div >
+            <div className="slide-item-container">
+              <img src={I1} alt="Imagem 1" className="circular-image" />
+            </div>
+          </div>
+          <div className="slider-item">
+            <div className="slide-item-container">
+              <img src={I2} alt="Imagem 2" className="circular-image" />
+            </div>
+          </div>
+          <div className="slider-item">
+            <div className="slide-item-container">
+              <img src={I3} alt="Imagem 3" className="circular-image" />
+            </div>
+          </div>
+          {/* Adicione mais imagens conforme necessário */}
+        </Slider>
+      </div>
+    );
+    
 };
 
 export default ImageSlider;

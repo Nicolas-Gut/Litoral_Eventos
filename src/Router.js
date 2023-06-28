cf386e110cab6c69de397161fc32caaf7c5082c2
Gabrielle1sbn <gabriellesbn15@gmail.com>
2023-06-28 16:16:19 -0300

    routes perfil

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Error404 from './pages/Error404'
 import Users from './pages/Users'
+import Perfil from './pages/Perfil'
 
 const Router = () => {
     return( 
@@ -9,6 +10,7 @@ const Router = () => {
             <Route path="/" element={<Home />} />
             <Route path="/users" element={<Users />} />
             <Route path="*" element={<Error404 />} />
+            <Route path="/perfil" element={<Perfil />}/>
         </Routes>
     )
 }

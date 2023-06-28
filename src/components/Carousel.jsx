9a8aed4cbb80c12420fc920c41b232ae1321c840
Gabrielle1sbn <gabriellesbn15@gmail.com>
2023-06-28 15:51:00 -0300

    mudei imagens

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,7 +4,8 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import foto1 from './assets/img/S1.jpg'
 import foto2 from './assets/img/S2.jpeg'
 import foto3 from './assets/img/S3.jpeg'
-import foto4 from './assets/img/I4.jpg'
+import foto4 from './assets/img/S4.png'
+import foto5 from './assets/img/S5.jpeg'
 
 const App = () => {
     return (
@@ -24,7 +25,11 @@ const App = () => {
           </div>
           <div>
             <img src={foto4} alt="Slide 4" />
-            <p className="legend">Slide Teste4</p>
+            <p className="legend">Carvalheira na Ladeira 24</p>
+          </div>
+          <div>
+            <img src={foto5} alt="Slide 5" />
+            <p className="legend">P1HARMONY</p>
           </div>
         </Carousel>
       </div>
